c391d03859041631cd0151d93bf3cbabda7d5786
Xavier Vello <xavier@bigblue.io>
2021-07-01 20:56:56 +0200

    npr: support dry run, fix git commands

--- new_pr.go
+++ new_pr.go
@@ -10,18 +10,19 @@ import (
 )
 
 type NewPrCmd struct {
-	Branch string `arg:"positional"`
+	Branch string `arg:"positional" help:"name of the branch to create"`
+	DryRun bool   `arg:"-n" help:"checkout and commit, but don't push'"`
 }
 
 func (c *NewPrCmd) Run(o *owl.Owl) error {
 	require.NotEmpty(o, c.Branch, "empty branch name")
 
 	// Ensure we have changes to commit
-	require.NotEmpty(o, o.Exec("git", "diff", "--shortstat", "HEAD"), "no changes to commit")
+	require.NotEmpty(o, o.Exec("git diff --shortstat HEAD"), "no changes to commit")
 
 	// Ensure we are on the default branch
-	currentBranch := o.Exec("git", "rev-parse", "--abbrev-ref", "HEAD")
-	defaultBranch := o.Exec("git", "rev-parse", "--abbrev-ref", "origin/HEAD")
+	currentBranch := o.Exec("git rev-parse --abbrev-ref HEAD")
+	defaultBranch := o.Exec("git rev-parse --abbrev-ref origin/HEAD")
 	require.Equal(o, strings.Split(defaultBranch, "/")[1], currentBranch, "not on default branch")
 
 	// Validate and prefix target branch name
@@ -44,8 +45,13 @@ func (c *NewPrCmd) Run(o *owl.Owl) error {
 
 	fmt.Printf("Creating and pushing new branch: %s\n", name)
 	o.Exec("git", "checkout", "-b", name)
-	o.Exec("git", "commit", "-a", "-o", name)
-	o.Exec("git", "push")
+	o.Exec("git", "commit", "-a", "-m", name)
+
+	if c.DryRun {
+		fmt.Println("Dry-run mode: not pushing branch")
+	} else {
+		o.Exec("git push")
+	}
 
 	return nil
 }

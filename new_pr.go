0d62e81ca57af311b59fe44192e77146e131c22c
Xavier Vello <xavier@bigblue.io>
2021-08-19 11:32:37 +0200

    npr: open github website, autofix when origin is not linked

--- new_pr.go
+++ new_pr.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
 	"regexp"
+	"runtime"
 	"strings"
 
 	"github.com/stretchr/testify/require"
@@ -18,6 +20,8 @@ type NewPrCmd struct {
 var (
 	linearBranchPattern  = regexp.MustCompile("^[0-9]{3,}-[a-z][a-z0-9-]+$")
 	genericBranchPattern = regexp.MustCompile("^(feature|fix|devops)/[a-z][a-z0-9-]+$")
+	githubRemotePattern  = regexp.MustCompile("git@github.com:([[:word:]]+\\/[[:word:]]+).git")
+	githubOpenPRURL      = "https://github.com/%s/compare/%s?expand=1"
 )
 
 func (c *NewPrCmd) Run(o owl.Owl) {
@@ -28,20 +32,38 @@ func (c *NewPrCmd) Run(o owl.Owl) {
 
 	// Ensure we are on the default branch
 	currentBranch := must.Exec(o, "git rev-parse --abbrev-ref HEAD")
+	if err := exec.Command("git", "rev-parse", "--abbrev-ref", "origin/HEAD").Run(); err != nil {
+		// Try to recover if the origin head is not already set
+		must.Exec(o, "git remote set-head --auto origin")
+	}
 	defaultBranch := must.Exec(o, "git rev-parse --abbrev-ref origin/HEAD")
 	require.Equal(o, strings.Split(defaultBranch, "/")[1], currentBranch, "not on default branch")
 
 	// Validate (and optionally prefix) target branch name
 	name := validateBranchName(o, c.Branch)
 
-	fmt.Printf("Creating and pushing new branch: %s\n", name)
+	o.Printf("Creating and pushing new branch: %s\n", name)
 	must.Exec(o, "git", "checkout", "-b", name)
 	must.Exec(o, "git", "commit", "-a", "-m", name)
 
 	if c.DryRun {
-		fmt.Println("Dry-run mode: not pushing branch")
+		o.Println("Dry-run mode: not pushing branch")
+		return
 	} else {
 		o.Println(must.Exec(o, "git", "push", "--set-upstream", "origin", name))
+	}
+
+	repository := githubRemotePattern.FindStringSubmatch(must.Exec(o, "git remote get-url origin"))
+	if len(repository) == 2 {
+		url := fmt.Sprintf(githubOpenPRURL, repository[1], name)
+		switch runtime.GOOS {
+		case "linux":
+			must.Exec(o, "xdg-open", url)
+		case "darwin":
+			must.Exec(o, "open", url)
+		default:
+			o.Printf("Open a PR at: %s\n", url)
+		}
 	}
 }
 
